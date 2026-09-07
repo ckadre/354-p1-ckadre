051a1c40ceb59f05255f2764426e6fcb266e0367
ckadre <cameronkadre@u.boisestate.edu>
2026-09-07 14:09:28 -0600

    9.7

--- bank/Bank.go
+++ bank/Bank.go
@@ -1,1 +1,19 @@
 package bank
+
+import (
+	"hash"
+	"github.com/ckadre/354-p1-ckadre/account"
+)
+
+type Bank struct {
+	accounts map[*IAccount]IAccount
+}
+
+func newBank()(b *Bank) {
+	b = new(Bank) 
+	b.accounts = Bank [account]account
+}
+
+func add(Account account) {
+	accounts.add(account)
+}
